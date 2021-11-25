b8ea2e75b600e21661671227da1acb766695afa6
Cian Gallagher <cian.gallagher@zendesk.com>
2021-11-25 20:52:30 +0000

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@
 # $3: filters
 # $4: appove
 
-export GITHUB_TOKEN=$2
-echo "Repo: $1, Filters: $3, appove: $4"
+export GITHUB_TOKEN=${2}
+echo "Repo: ${1}, Filters: ${3}, appove: ${4}"
 
 if [[ "$3" != "" && "$4" != "" ]]; then
     if [[ $3 != "false" ]]; then
